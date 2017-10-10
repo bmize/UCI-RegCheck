d543efe41df410d5e078e91eb719b9d83a12e445
bmize14 <19563823+bmize14@users.noreply.github.com>
2017-10-09 19:18:12 -0700

    input_handler.py creation
    Added basic structure of the InputHandler class

---
+++ UCI-RegCheck/controller/input_handler.py
@@ -0,0 +1,23 @@
+# input.py
+from model import course_scraper
+from view import menu
+
+
+class InputHandler:
+    def __init__(self):
+        # self._input = None
+        self._command_list = {'1', '2', '3'}
+        self._course_scraper = course_scraper.CourseScraper
+
+
+    def receive_input(self):
+        menu.display_welcome_message(self._course_scraper.)
+
+        while True:
+            menu.display_main_menu()
+            user_input = input()
+
+
+    def handle_input(self) -> None:
+        # handle input
+        return None
