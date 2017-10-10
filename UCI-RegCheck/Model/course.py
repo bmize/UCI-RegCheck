100827fc17cf8847459d4d71385a36e4e79e4ff0
bmize14 <19563823+bmize14@users.noreply.github.com>
2017-10-09 19:00:23 -0700

    course.py semantic change
    Changed private variables to begin with one underscore rather than two

--- UCI-RegCheck/Model/course.py
+++ UCI-RegCheck/Model/course.py
@@ -3,17 +3,17 @@
 
 class Course:
     def __init__(self, course_code, students_max):
-        self.__course_code = course_code
-        self.__students_max = students_max
+        self._course_code = course_code
+        self._students_max = students_max
 
     def set_course_code(self, course_code: int):
-        self.__course_code = course_code
+        self._course_code = course_code
 
     def set_students_max(self, students_max: int):
-        self.__students_max = students_max
+        self._students_max = students_max
 
     def get_course_code(self) -> int:
-        return self.__course_code
+        return self._course_code
 
     def get_students_max(self) -> int:
-        return self.__students_max
+        return self._students_max
