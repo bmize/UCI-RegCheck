2521efd6a72a73bb72451bbe121b696170fb7930
bmize14 <19563823+bmize14@users.noreply.github.com>
2017-10-09 19:05:54 -0700

    menu.py creation
    Added menu.py
    
    Added initial helper functions to display menus to the user

---
+++ UCI-RegCheck/view/menu.py
@@ -0,0 +1,18 @@
+# menu.py
+# Contains helper functions for displaying menus and other messages to the user.
+
+
+DEFAULT_TIME_DELAY_IN_SECONDS = 600
+
+
+def display_welcome_message() -> None:
+    print("Welcome to UCI RegCheck! Enter a number corresponding to a menu command to continue.\n\n")
+
+
+def display_main_menu(time_delay: int) -> None:
+    main_menu = "Select a menu option:\n" \
+                "  1. Add new course to monitor\n" \
+                "  2. Edit time delay between updates (current: {} seconds)\n" \
+                "  3. Quit\n"
+
+    print(main_menu.format(time_delay))
