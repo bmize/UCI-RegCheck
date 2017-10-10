4ee83dfff7cff11a945413f4fe759d87e7b0d4b2
bmize14 <19563823+bmize14@users.noreply.github.com>
2017-10-09 19:16:12 -0700

    course.py rework
    Adjusted the Course class to be more Pythonic

--- UCI-RegCheck/Model/course.py
+++ UCI-RegCheck/Model/course.py
@@ -3,17 +3,5 @@
 
 class Course:
     def __init__(self, course_code, students_max):
-        self._course_code = course_code
-        self._students_max = students_max
-
-    def set_course_code(self, course_code: int):
-        self._course_code = course_code
-
-    def set_students_max(self, students_max: int):
-        self._students_max = students_max
-
-    def get_course_code(self) -> int:
-        return self._course_code
-
-    def get_students_max(self) -> int:
-        return self._students_max
+        self.course_code = course_code
+        self.students_max = students_max
