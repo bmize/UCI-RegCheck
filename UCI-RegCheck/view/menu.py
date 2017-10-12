9097e984af93d451f7f52885381dae72828a7e9a
bmize14 <19563823+bmize14@users.noreply.github.com>
2017-10-12 02:00:28 -0700

    menu.py documentation and string clean-up
    Cleaned up line lengths to prevent surpassing 80 characters/line
    
    Added additional documentation

--- UCI-RegCheck/view/menu.py
+++ UCI-RegCheck/view/menu.py
@@ -1,15 +1,20 @@
-# menu.py
-# Contains helper functions for displaying menus and other messages to the user.
+"""menu.py
+
+Contains helper functions for displaying menus and other messages to the user.
+"""
 
 
-def display_welcome_message() -> None:
-    print("Welcome to UCI RegCheck! Enter a number corresponding to a menu command to continue.\n\n")
+def display_welcome_message():
+    """Prints a welcome message to be displayed at the program's start."""
+    print("Welcome to UCI RegCheck! Enter a number corresponding to a menu"
+          " command to continue.\n\n")
 
 
-def display_main_menu(time_delay: int) -> None:
-    main_menu = "Select a menu option:\n" \
-                "  1. Add new course to monitor\n" \
-                "  2. Edit time delay between updates (current: {} seconds)\n" \
-                "  3. Quit\n"
+def display_main_menu(time_delay: int):
+    """Prints a formatted main menu to be displayed to the user."""
+    main_menu = "Select a menu option:\n"
+    "  1. Add new course to monitor\n"
+    "  2. Edit time delay between updates (current: {} seconds)\n"
+    "  3. Quit\n"
 
     print(main_menu.format(time_delay))
