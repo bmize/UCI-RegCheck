20f656c91663729d28b610d20048257bd3b160e1
bmize14 <19563823+bmize14@users.noreply.github.com>
2017-10-12 01:55:55 -0700

    input_handler.py documentation clean-up
    Added more thorough and accurate documentation
    
    Added todo statement

--- UCI-RegCheck/controller/input_handler.py
+++ UCI-RegCheck/controller/input_handler.py
@@ -1,23 +1,46 @@
-# input.py
-from model import course_scraper
+"""input_handler.py
+
+Contains the InputHandler class and methods used for interpreting user input
+ and calling the appropriate function/method.
+"""
+
+from model.course_scraper import CourseScraper
 from view import menu
 
 
 class InputHandler:
-    def __init__(self):
-        # self._input = None
-        self._command_list = {'1', '2', '3'}
-        self._course_scraper = course_scraper.CourseScraper()
+    """Contains methods to read and handle user input."""
 
+    def __init__(self):
+        """
+        :param _commands: Set of integers corresponding to the numbers in
+         view.menu.display_main_menu()
+        :param _course_scraper: CourseScraper object used for gathering and
+         sharing course information.
+        """
+        self._commands = {'1', '2', '3'}
+        self._course_scraper = CourseScraper()
 
     def receive_input(self):
+        """Primary loop for continuously receiving user input until program
+         is exited.
+
+        :return: None
+        """
         menu.display_welcome_message()
 
         while True:
             menu.display_main_menu(self._course_scraper.delay_in_seconds)
             user_input = input()
+            self.handle_input(user_input)
 
+    def handle_input(self, user_input: str):
+        """Calls appropriate function/method depending on user_input. Uses
+         self._commands to tie user_input to menu options in
+         view.menu.display_main_menu()
 
-    def handle_input(self) -> None:
-        # handle input
-        return None
+        :param user_input: str correlating to menu options in
+         view.menu.display_main_menu()
+        :return: None
+        """
+        # TODO: check user_input and perform appropriate action
