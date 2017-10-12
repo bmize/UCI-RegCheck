d21292b3719506f73020e9a05e367ff367d1fe6f
bmize14 <19563823+bmize14@users.noreply.github.com>
2017-10-12 01:59:15 -0700

    course.py added documentation
    Added additional, more accurate documentation

--- UCI-RegCheck/Model/course.py
+++ UCI-RegCheck/Model/course.py
@@ -1,7 +1,23 @@
-# Class representing the basic characteristics of a course
+"""course.py
+
+Contains the Course class which is used to represent the basic characteristics
+ of a UCI course
+"""
 
 
 class Course:
-    def __init__(self, course_code, students_max):
+    """Represents a Course object"""
+
+    def __init__(self, course_code):
+        """
+        :param course_code: int representing a specific course offered by
+         UCI. For example, 36595 in the Fall Quarter of 2017 represents the
+         course ICS 31.
+        :param students_current: int representing the current number of
+         students enrolled in this course
+        :param students_max: int representing the maximum number of
+         students enrolled in this course
+        """
         self.course_code = course_code
-        self.students_max = students_max
+        self.students_current = None
+        self.students_max = None
