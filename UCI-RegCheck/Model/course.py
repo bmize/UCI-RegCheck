032ad11428ca1f645dfc429ff137dcdf7a2b45d1
bmize14 <19563823+bmize14@users.noreply.github.com>
2017-10-12 02:34:41 -0700

    documentation cleanup
    Cleaned up documentation to more appropriately match Google's style guide

--- UCI-RegCheck/Model/course.py
+++ UCI-RegCheck/Model/course.py
@@ -1,22 +1,28 @@
 """course.py
 
 Contains the Course class which is used to represent the basic characteristics
- of a UCI course
+of a UCI course
 """
 
 
 class Course:
-    """Represents a Course object"""
+    """Represents a Course object
+
+    Attributes:
+        course_code: int representing a specific course offered by UCI. For
+            example, 36595 in the Fall Quarter of 2017 represents the course
+            ICS 31.
+        students_current: int representing the current number of students
+            enrolled in this course
+        students_max: int representing the maximum number of students
+            enrolled in this course
+    """
 
     def __init__(self, course_code):
         """
-        :param course_code: int representing a specific course offered by
-         UCI. For example, 36595 in the Fall Quarter of 2017 represents the
-         course ICS 31.
-        :param students_current: int representing the current number of
-         students enrolled in this course
-        :param students_max: int representing the maximum number of
-         students enrolled in this course
+        :param course_code: int representing a specific course offered by UCI.
+            For example, 36595 in the Fall Quarter of 2017 represents the
+            course ICS 31.
         """
         self.course_code = course_code
         self.students_current = None
--- UCI-RegCheck/controller/input_handler.py
+++ UCI-RegCheck/controller/input_handler.py
@@ -1,7 +1,7 @@
 """input_handler.py
 
 Contains the InputHandler class and methods used for interpreting user input
- and calling the appropriate function/method.
+and calling the appropriate function/method.
 """
 
 from model.course_scraper import CourseScraper
@@ -9,21 +9,22 @@ from view import menu
 
 
 class InputHandler:
-    """Contains methods to read and handle user input."""
+    """Contains methods to read and handle user input.
+
+    Attributes
+        _commands: Set of integers corresponding to the numbers in
+            view.menu.display_main_menu()
+        _course_scraper: CourseScraper object used for gathering and
+            sharing course information.
+    """
 
     def __init__(self):
-        """
-        :param _commands: Set of integers corresponding to the numbers in
-         view.menu.display_main_menu()
-        :param _course_scraper: CourseScraper object used for gathering and
-         sharing course information.
-        """
         self._commands = {'1', '2', '3'}
         self._course_scraper = CourseScraper()
 
     def receive_input(self):
-        """Primary loop for continuously receiving user input until program
-         is exited.
+        """Primary loop for continuously receiving user input until program is
+        exited.
 
         :return: None
         """
@@ -36,11 +37,11 @@ class InputHandler:
 
     def handle_input(self, user_input: str):
         """Calls appropriate function/method depending on user_input. Uses
-         self._commands to tie user_input to menu options in
-         view.menu.display_main_menu()
+        self._commands to tie user_input to menu options in
+        view.menu.display_main_menu()
 
         :param user_input: str correlating to menu options in
-         view.menu.display_main_menu()
+            view.menu.display_main_menu()
         :return: None
         """
         # TODO: check user_input and perform appropriate action
